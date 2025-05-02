465f49a2c2574e25dca51e79278b4c4defc2eaf6
Mubasshir <mubaasshiralkasshaf22@gmail.com>
2025-05-02 15:07:48 +0600

    Add Cors Origins

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandlers";
 import httpStatus from "http-status";
 
 const app: Application = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -20,16 +25,15 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/", router);
 app.use(globalErrorHandler);
 
-
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-      success: false,
-      message: "Api Not Found!",
-      error: {
-        path: req.originalUrl,
-        message: "Your requested path is not found!",
-      },
-    });
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Api Not Found!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
   });
-  
-  export default app;
+});
+
+export default app;
